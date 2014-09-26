af1c28efbdcff57e3e18f45d4f5c940d5ee893c7
Sam Lendle <sam.lendle@gmail.com>
2014-09-26 00:11:06 -0700

    add some tests for LReg

---
+++ test/LReg.jl
@@ -0,0 +1,25 @@
+module TestLReg
+
+import TargetedLearning.LReg
+
+srand(10)
+x = rand(50, 3)
+y = round(rand(50))
+
+lrfit = LReg.lreg(x, y)
+@test isa(lrfit, LReg.LR)
+
+splrfit = LReg.sparselreg(x, y, [1:3])
+@test isa(splrfit, LReg.SSLR)
+@test lrfit.beta == splrfit.beta
+
+sslrfit = LReg.sslreg(x, y, [1:3])
+@test isa(sslrfit, LReg.SSLR)
+@test lrfit.beta == sslrfit.beta
+
+sp2 = LReg.sparselreg(x, y, [1,3])
+@test sp2.beta[[1,3]] == LReg.lreg(x[:, [1,3]], y).beta
+@test sp2.beta[2] == 0.0
+@test sp2.beta == LReg.sslreg(x, y, [1,3]).beta
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using TargetedLearning
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+include("LReg.jl")
