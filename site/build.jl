07fd10215cb700586bdc3f24ff638256b5f0041a
Sam Lendle <sam.lendle@gmail.com>
2015-02-26 11:17:03 -0800

    Remove ./site from master

--- site/build.jl
+++
@@ -1,9 +0,0 @@
-using Lexicon, TargetedLearning
-
-fmnames = [("ctmles", TargetedLearning.CTMLEs),
-           ("strategies", TargetedLearning.CTMLEs.Strategies),
-           ("lreg", TargetedLearning.LReg)]
-
-for (fname, mod) in fmnames
-	save("docs/api/$fname.md", mod, mathjax=true)
-end
